9519799806c367ed2eec01ea2096b0622bec0073
tkianai <tkianai@163.com>
2019-09-21 11:03:27 +0800

    fixed typo error

--- models/base_model.py
+++ models/base_model.py
@@ -13,7 +13,7 @@ class BaseModel(nn.Module):
         self.opt = opt
         self.gpu_ids = opt.gpu_ids
         self.isTrain = opt.isTrain
-        self.Tensor = torch.cuda.FloatTensor() if self.gpu_ids else torch.Tensor
+        self.Tensor = torch.cuda.FloatTensor if self.gpu_ids else torch.Tensor
         self.save_dir = os.path.join(opt.checkpoints_dir, opt.name)
 
 
