81a60c02be08a22ef3d5a1fd49ccd1cdf04d1ecf
tkianai <tkianai@163.com>
2019-09-22 08:28:16 +0800

    modify visulizer

--- utils/visulizer.py
+++ utils/visulizer.py
@@ -34,66 +34,58 @@ class Visualizer():
             log_file.write(
                 '======== Training Loss ({}) ========\n'.format(now))
 
-    def display_current_results(self, visuals, epoch, step):
+    def display_current_results(self, visuals, size, epoch, iteration):
         if self.tboard:
             img_summaries = []
             for label, image_numpy in visuals.items():
                 self.writer.add_image(
-                    "Epoch{}step-{}-{}.jpg".format(epoch, step, label), image_numpy, dataformats='HWC')
+                    "size-{}-epoch-{}-iter-{}-{}.jpg".format(size, epoch, iteration, label), image_numpy, dataformats='HWC')
 
         if self.use_html:
             for label, image_numpy in visuals.items():
                 if isinstance(image_numpy, list):
                     for i in range(len(image_numpy)):
                         img_path = os.path.join(
-                            self.img_dir, "epoch{}_{}_{}.jpg".format(epoch, label, i))
+                            self.img_dir, "size-{}-epoch-{}-iter-{}-{}_{}.jpg".format(size, epoch, iteration, label, i))
                         misc.save_image(image_numpy[i], img_path)
 
                 else:
                     img_path = os.path.join(
-                        self.img_dir, "epoch{}_{}.jpg".format(epoch, label))
+                        self.img_dir, "size-{}-epoch-{}-iter-{}-{}.jpg".format(size, epoch, iteration, label))
                     misc.save_image(image_numpy, img_path)
 
             # update webpage
-            webpage = html.HTML(
-                self.web_dir, "Experiment name = {}".format(self.name), refresh=30)
-            for n in range(epoch, 0, -1):
-                webpage.add_header("epoch [{}]".format(n))
-                ims = []
-                txts = []
-                links = []
-
-                for label, image_numpy in visuals.items():
-                    if isinstance(image_numpy, list):
-                        for i in range(len(image_numpy)):
-                            img_path = "epoch{}_{}_{}.jpg".format(n, label, i)
-                            ims.append(img_path)
-                            txts.append(label + str(i))
-                            links.append(img_path)
-                    else:
-                        img_path = "epoch{}_{}.jpg".format(n, label)
-                        ims.append(img_path)
-                        txts.append(label)
-                        links.append(img_path)
-
-                if len(ims) < 10:
+            webpage = html.HTML(self.web_dir, "Experiment name = {}".format(self.name), refresh=30)
+            images = sorted(os.listdir(self.img_dir), reverse=True)
+            webpage.add_header("Training process visulization".format(n))
+            ims = []
+            txts = []
+            links = []
+            for image in images:
+                ims.append(image)
+                links.append(image)
+                label = image.split('.')[0].split('_')[0].split('-')[-1]
+                label += ": " + image.split('.')[0].replace('_', '@')
+                txts.append(label)
+            
+            if len(ims) < 10:
                     webpage.add_images(ims, txts, links, width=self.win_size)
-                else:
-                    num = int(round(len(ims) / 2.0))
-                    webpage.add_images(
-                        ims[:num], txts[:num], links[:num], width=self.win_size)
-                    webpage.add_images(
-                        ims[num:], txts[num:], links[num:], width=self.win_size)
+            else:
+                num = int(round(len(ims) / 2.0))
+                webpage.add_images(
+                    ims[:num], txts[:num], links[:num], width=self.win_size)
+                webpage.add_images(
+                    ims[num:], txts[num:], links[num:], width=self.win_size)
 
             webpage.save()
 
-    def plot_current_errors(self, errors, step):
+    def plot_current_errors(self, errors, iteration):
         if self.tboard:
             for tag, value in errors.items():
-                self.writer.add_scalar(tag, value, step)
+                self.writer.add_scalar(tag, value, iteration)
 
-    def print_current_errors(self, epoch, i, errors, t):
-        msg = "(epoch: {}, iters: {}, time: {:.3f})".format(epoch, i, t)
+    def print_current_errors(self, size, epoch, iteration, errors, t):
+        msg = "(size: {}, epoch: {}, iters: {}, time: {:.3f})".format(size, epoch, iteration, t)
         for k, v in errors.items():
             if v != 0:
                 msg += "{}: {:.3f}".format(k, v)
@@ -101,24 +93,3 @@ class Visualizer():
         print(msg)
         with open(self.log_name, "a") as log_file:
             log_file.write("{}\n".format(msg))
-
-    # save image to the disk
-    def save_images(self, webpage, visuals, image_path):
-        image_dir = webpage.get_image_dir()
-        short_path = ntpath.basename(image_path[0])
-        name = os.path.splitext(short_path)[0]
-
-        webpage.add_header(name)
-        ims = []
-        txts = []
-        links = []
-
-        for label, image_numpy in visuals.items():
-            image_name = '%s_%s.jpg' % (name, label)
-            save_path = os.path.join(image_dir, image_name)
-            util.save_image(image_numpy, save_path)
-
-            ims.append(image_name)
-            txts.append(label)
-            links.append(image_name)
-        webpage.add_images(ims, txts, links, width=self.win_size)
